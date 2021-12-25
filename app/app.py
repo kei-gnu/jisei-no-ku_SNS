6cea1594bba3397ddf66f3fb38ae8a711dbc68c7
tomita <319012kei@users.noreply.github.com>
2021-12-25 17:32:51 +0900

    update app

--- app/app.py
+++ app/app.py
@@ -102,5 +102,5 @@ def datestr_fiter(s):
     time.localtime(s))
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=3000)
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run()
