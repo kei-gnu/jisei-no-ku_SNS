822ba6b7ddfad62a052605a67aad8f2fb31e2eba
tomita <319012kei@users.noreply.github.com>
2021-12-27 13:58:04 +0900

    Initial commit

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ import sns_user as user, sns_data as data
 app = Flask(__name__)
 app.secret_key = 'TIIDe5TUMtPUHpyu'
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 # --- URLのルーティング 
 @app.route('/') 
 @user.login_required
